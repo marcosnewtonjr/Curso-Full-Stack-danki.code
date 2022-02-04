bcb5b03eac95ef4608b2b633b65722ed8a2c2cc2
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-04 16:04:31 -0300

    Aplicação de Repetição

--- JavaScript/aula13/ambiente.js
+++ JavaScript/aula13/ambiente.js
@@ -1,10 +1,20 @@
 // Aplicação de Repetição
 
 var c = 1
+
+/*
+Teste no início
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c++
 }
+*/
+
+// Teste no final
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
 
 /*
 console.log('Tudo bem?')
