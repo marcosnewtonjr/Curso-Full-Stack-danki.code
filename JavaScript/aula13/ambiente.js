47fd612d763235cab7b3a644da7ace9dec386f78
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-04 16:05:35 -0300

    Update

--- JavaScript/aula13/ambiente.js
+++ JavaScript/aula13/ambiente.js
@@ -2,13 +2,11 @@
 
 var c = 1
 
-/*
-Teste no início
-while (c <= 6) {
-    console.log(`Passo ${c}`)
-    c++
-}
-*/
+// Teste no início
+// while (c <= 6) {
+//    console.log(`Passo ${c}`)
+//    c++
+// }
 
 // Teste no final
 do {
