ab2310232eec5618c78dff939ee542405b01d3e9
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-05 22:24:30 -0300

    Variáveis Compostas

---
+++ JavaScript/aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5, 8, 2, 9, 3]
+num.sort() // Comando para colocar os valores em ordem
+// num[5] = 15 para adicionar um valor na posição determinada
+
+num.push(15) // Comando para inserir um valor na ultima posição sem saber qual é
+console.log(num)
+
+let comprimento = num.length
+console.log(comprimento)
+
